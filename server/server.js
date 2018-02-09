37362e8d79d3ae6ae7faa2737c8bb3c0cffe6ecb
Kyrychenko <darya.kyrychenko@ekreative.com>
2018-02-09 12:24:00 +0200

    emit newmessage and createmessage

--- server/server.js
+++ server/server.js
@@ -1,14 +1,39 @@
 const path = require('path')
 const express = require('express')
+const http = require('http')
+const socketIO = require('socket.io')
 
 const publicPath = path.join(__dirname, '../public')
 
-let app = express()
 let port = process.env.PORT || 3000
+let app = express()
+let server = http.createServer(app)
+let io = socketIO(server)
 
 app.use(express.static(publicPath))
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('new user connected')
+
+  socket.on('connect', () => {
+    console.log('User connect')
+  })
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message)
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    })
+  })
+
+  socket.on('disconnect', () => {
+    console.log('Disconnected')
+  })
+})
+
+server.listen(port, () => {
   console.log('Started on port ', port)
 })
 
