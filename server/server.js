76451d2274a83c7a4a6d9c7f1c7948fd2d2512b2
Kyrychenko <darya.kyrychenko@ekreative.com>
2018-02-11 16:24:08 +0200

    greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -2,12 +2,11 @@ const path = require('path')
 const http = require('http')
 const socketIO = require('socket.io')
 const express = require('express')
-const http = require('http')
-const socketIO = require('socket.io')
 
 const publicPath = path.join(__dirname, '../public')
 
 let port = process.env.PORT || 3000
+let app = express()
 let server = http.createServer(app)
 let io = socketIO(server)
 
@@ -17,10 +16,21 @@ io.on('connection', (socket) => {
   console.log('NEw user connected')
 
   socket.emit('newMessage', {
-    from: 'mike@example.com',
-    text: 'Hey',
-    createdAt: 123
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
   })
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  })
+  // socket.emit('newMessage', {
+  //   from: 'mike@example.com',
+  //   text: 'Hey',
+  //   createdAt: 123
+  // })
 
   socket.on('createMessage', (data) => {
     console.log('createMessage', data)
@@ -30,6 +40,12 @@ io.on('connection', (socket) => {
       text: data.text,
       createdAt: new Date().getTime()
     })
+    //
+    // socket.broadcast.emit('newMessage', {
+    //   from: data.from,
+    //   text: data.text,
+    //   createdAt: new Date().getTime()
+    // })
   })
 
   socket.on('disconnect', () => {
